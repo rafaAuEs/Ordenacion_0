6da9e27ad6716ac917301d786e03bd8123dd6896
rafaAuEs <rafaelaulaestudio@gmail.com>
2025-10-08 17:24:11 +0200

    Primer commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Integer> arrayNumeros = new ArrayList<>();
+        arrayNumeros.add(346);
+        arrayNumeros.add(8498);
+        arrayNumeros.add(12543);
+        arrayNumeros.add(35342);
+        arrayNumeros.add(2135);
+        arrayNumeros.add(578);
+        arrayNumeros.add(395349);
+        arrayNumeros.add(436547);
+        arrayNumeros.add(21545);
+        arrayNumeros.add(55486);
+    }
+
+}
---
+++ src/SortingThread.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+public class SortingThread extends Thread{
+    public ArrayList<Integer> arrayNumeros;
+    public String nombreAlgoritmo;
+    public long tiempoEjecucion;
+
+    public SortingThread(ArrayList<Integer> arrayNumeros, String nombre){
+        this.arrayNumeros = new ArrayList<>(arrayNumeros);
+        this.nombreAlgoritmo = nombre;
+    }
+
+    public abstract void ordenar();
+
+    @Override
+    public void run(){
+        long inicio = System.currentTimeMillis();
+            ordenar();
+            long fin = System.currentTimeMillis();
+            this.tiempoEjecucion = fin - inicio;
+
+            System.out.println(this.nombreAlgoritmo + "finalizo en: " + this.tiempoEjecucion);
+    }
+}
